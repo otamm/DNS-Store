384c04715fa308a685b408bf34590b2956f52acc
Otavio <mrotamm@gmail.com>
2019-11-11 19:38:43 -0500

    basic model body filling

--- app/models/dns_record.rb
+++ app/models/dns_record.rb
@@ -1,2 +1,15 @@
 class DnsRecord < ApplicationRecord
+	# ip_address->string
+	has_and_belongs_to_many :hostnames
+
+	IPV4_REGEX = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/
+	validates_format_of :ip_address, with: IPV4_REGEX
+	
+	before_save :include_itself_in_hostnames
+
+	def include_itself_in_hostnames
+		hostnames.each do |hostname|
+			hostname.dns_records << self if !hostname.dns_records.include? self
+		end
+	end
 end
--- app/models/hostname.rb
+++ app/models/hostname.rb
@@ -1,2 +1,15 @@
 class Hostname < ApplicationRecord
+	# address->string
+	has_and_belongs_to_many :dns_records
+
+	DOMAIN_NAME_REGEX = /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/
+	validates_format_of :ip_address, with: DOMAIN_NAME_REGEX
+
+	before_save :include_itself_in_dns_records
+
+	def include_itself_in_dns_records
+		dns_records.each do |dns_record|
+			dns_record.hostnames << self if !dns_record.hostnames.include? self
+		end
+	end
 end
