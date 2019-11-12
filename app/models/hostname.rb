3db364a1cd28da6c4b80c9351d4aa82daefa9ec2
Otavio <mrotamm@gmail.com>
2019-11-11 20:12:20 -0500

    corrected multiline bug

--- app/models/hostname.rb
+++ app/models/hostname.rb
@@ -2,7 +2,7 @@ class Hostname < ApplicationRecord
 	# address->string
 	has_and_belongs_to_many :dns_records
 
-	DOMAIN_NAME_REGEX = /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/
+	DOMAIN_NAME_REGEX = /\A((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})\z/
 	validates_format_of :ip_address, with: DOMAIN_NAME_REGEX
 
 	before_save :include_itself_in_dns_records
