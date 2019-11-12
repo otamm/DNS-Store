6b0a6e0043a2a9a5730c819f520a1b0f80e233bb
Otavio <mrotamm@gmail.com>
2019-11-12 12:47:35 -0500

    finished model specs

--- spec/models/dns_record_spec.rb
+++ spec/models/dns_record_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe DnsRecord, type: :model do
   describe "validates attributes" do
   	before(:all) do
   		@invalid_record = build(:dns_record)
+  		@invalid_record.ip_address = "abc!!!"
   	end
   	before(:each) do
   		@dns_record_1 = build(:dns_record)
@@ -13,13 +14,22 @@ RSpec.describe DnsRecord, type: :model do
   	end
 
   	it "validates an IPV4 address before saving" do
-  		@invalid_record.ip_address = "abc!!!"
   		expect(@invalid_record.save).to eq false
   	end
   	it "adds itself as an index to hostname model automatically" do
   		@dns_record_1.hostnames << @hostname_1
   		@dns_record_1.save
   		expect(@hostname_1.dns_records.include? @dns_record_1).to eq true
+  	end
+  	it "allows for multiple association of hostnames and dns records" do
+  		@dns_record_1.hostnames << @hostname_1
+  		@dns_record_1.hostnames << @hostname_2
+  		@dns_record_2.hostnames << @hostname_1
+  		@dns_record_2.hostnames << @hostname_2
+  		@dns_record_1.save # all of the above should be persisted by this point
+  		h1 = @dns_record_1.hostnames
+  		h2 = @dns_record_2.hostnames
+  		expect(h1.include?(@hostname_1) && h1.include?(@hostname_2) && h2.include?(@hostname_1) && h2.include?(@hostname_2)).to eq true
   	end
   end
 end
--- spec/models/hostname_spec.rb
+++ spec/models/hostname_spec.rb
@@ -2,11 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Hostname, type: :model do
   describe "validates attributes" do
-  	it "validates domain name" do
+  	before(:all) do
+  		@invalid_record = build(:hostname)
+  		@invalid_record.address = "facebook-!!.net"
+  	end
+  	before(:each) do
+  		@dns_record_1 = build(:dns_record)
+  		@dns_record_2 = build(:dns_record)
+  		@hostname_1 = build(:hostname)
+  		@hostname_2 = build(:hostname)
+  	end
 
+  	it "validates domain name" do
+  		expect(@invalid_record.save).to eq false
   	end
   	it "adds itself as an index to dns record model automatically" do
-
+  		@hostname_1.dns_records << @dns_record_1
+  		@hostname_1.save
+  		expect(@dns_record_1.hostnames.include? @hostname_1).to eq true
+  	end
+  	it "allows for multiple association of hostnames and dns records" do
+  		@hostname_1.dns_records << @dns_record_1
+  		@hostname_1.dns_records << @dns_record_2
+  		@hostname_2.dns_records << @dns_record_1
+  		@hostname_2.dns_records << @dns_record_2
+  		@hostname_1.save # all of the above should be persisted by this point
+  		d1 = @hostname_1.dns_records
+  		d2 = @hostname_2.dns_records
+  		expect(d1.include?(@dns_record_1) && d1.include?(@dns_record_2) && d2.include?(@dns_record_1) && d2.include?(@dns_record_2)).to eq true
   	end
   end
 end
